2b8713c28f996707a8fa8b5eea235e4729ce21e1
Sławomir Borczyk <slawek_python@outlook.com>
2021-12-14 23:38:11 +0100

    Obliczenia pierwszy miesiąc

--- pythonzadanie2.py
+++ pythonzadanie2.py
@@ -2,7 +2,7 @@
 
 print ("Podaj wartość początkową kredytu")
 
-kredyt = input()
+kredyt = int(input())
 
 print("Podaj oprocentowanie kredytu")
 
@@ -10,8 +10,18 @@ oprocentowanie = input()
 
 print ("Podaj kwotę raty")
 
-rata = input()
+rata = int(input())
 
+#=(1 + ((B10+3(oprocentowanie)/1200)) * C9 - 200
 
+message = ("Twoja pozostała kwota kredytu to X, to Y mniej niż w poprzednim miesiącu.")
+inflacja = 1.592824484
+#suma = (1 + ((inflacja + oprocentowanie)//1200) * kredyt - rata
 
+suma =  (1 + ((inflacja + float(oprocentowanie))/1200)) * kredyt - rata
 
+lastmonth = kredyt - suma
+
+print("Twoja pozostała kwota kredytu to " + str(suma))
+print("to " + str(lastmonth))
+print("mniej niż w poprzednim miesiacu")
