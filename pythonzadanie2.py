08aaf53b5e2c9a125da4456cd00b4537efabb52c
Sławomir Borczyk <slawek_python@outlook.com>
2021-12-14 22:08:44 +0100

    Stworzenie pliku i podstawowych danych

---
+++ pythonzadanie2.py
@@ -0,0 +1,17 @@
+#Obliczanie aktualnej wartości kredytu
+
+print ("Podaj wartość początkową kredytu")
+
+kredyt = input()
+
+print("Podaj oprocentowanie kredytu")
+
+oprocentowanie = input()
+
+print ("Podaj kwotę raty")
+
+rata = input()
+
+
+
+
