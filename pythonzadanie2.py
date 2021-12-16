e6767982ce9a2d785d7ac847f9371eb61a139c33
Sławomir Borczyk <slawek_python@outlook.com>
2021-12-16 23:30:33 +0100

    Poprawienie kodu 1) zmiana int na float 2) Poprawienie komunikatu końcowego.

--- pythonzadanie2.py
+++ pythonzadanie2.py
@@ -1,30 +1,27 @@
 #Obliczanie aktualnej wartości kredytu
+#message = ("Twoja pozostała kwota kredytu to X, to Y mniej niż w poprzednim miesiącu.")
+#=(1 + ((B10+3(oprocentowanie)/1200)) * C9 - 200
 
 print ("Podaj wartość początkową kredytu")
 
-kredyt = int(input())
+kredyt = float(input())
 
 print("Podaj oprocentowanie kredytu")
 
-oprocentowanie = input()
+oprocentowanie = float(input())
 
-print ("Podaj kwotę raty")
+print("Podaj kwotę raty")
 
-rata = int(input())
+rata = float(input())
 
-#=(1 + ((B10+3(oprocentowanie)/1200)) * C9 - 200
-
-message = ("Twoja pozostała kwota kredytu to X, to Y mniej niż w poprzednim miesiącu.")
 inflacja = 1.592824484
 #suma = (1 + ((inflacja + oprocentowanie)//1200) * kredyt - rata
 
-suma =  (1 + ((inflacja + float(oprocentowanie))/1200)) * kredyt - rata
+suma =  (1 + ((inflacja + (oprocentowanie))/1200)) * kredyt - rata
 
 lastmonth = kredyt - suma
 
-print("Twoja pozostała kwota kredytu to " + str(suma))
-print("to " + str(lastmonth))
-print("mniej niż w poprzednim miesiacu \n")
+print("Twoja pozostała kwota kredytu to ", str(suma),"to ", str(lastmonth), "mniej niż w poprzednim miesiacu \n")
 
 inflacja = -0.453509101
 
