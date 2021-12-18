ec9ad73a62aff3243ce4b0b78c3d52f42b79cb4a
Sławomir Borczyk <slawek_python@outlook.com>
2021-12-18 17:32:46 +0100

    Zaokrąglenie wyświetlanych kwot i poprawienie komenty print

--- pythonzadanie2.py
+++ pythonzadanie2.py
@@ -17,59 +17,35 @@ rata = float(input())
 inflacja = 1.592824484
 #suma = (1 + ((inflacja + oprocentowanie)//1200) * kredyt - rata
 
-suma =  (1 + ((inflacja + (oprocentowanie))/1200)) * kredyt - rata
+suma =  round((1 + ((inflacja + (oprocentowanie))/1200)) * kredyt - rata, 2)
 
-lastmonth = kredyt - suma
+lastmonth = round(kredyt - suma, 2)
 
-print("Twoja pozostała kwota kredytu to ", str(suma),"to ", str(lastmonth), "mniej niż w poprzednim miesiacu \n")
+#print("Twoja pozostała kwota kredytu to ", str(suma),"to ", str(lastmonth), "mniej niż w poprzednim miesiacu")
+print(f"Twoja pozostała kwota kredytu to {suma}, to {lastmonth} mniej niż w poprzednim miesiącu. \n")
 
 inflacja = -0.453509101
-
-suma_luty =  (1 + ((inflacja + float(oprocentowanie))/1200)) * suma - rata
-
-lastmonth = suma - suma_luty
-
-print("Twoja pozostała kwota kredytu to " + str(suma_luty))
-print("to " + str(lastmonth))
-print("mniej niż w poprzednim miesiacu \n")
+suma_luty =  round((1 + ((inflacja + float(oprocentowanie))/1200)) * suma - rata, 2)
+lastmonth = round(suma - suma_luty, 2)
+#print("Twoja pozostała kwota kredytu to ", str(suma_luty),"to ", str(lastmonth), "mniej niż w poprzednim miesiacu \n")
+print("Twoja pozostała kwota kredytu to {}, to {} mniej niż w poprzednim miesiąciu. \n".format(suma_luty, lastmonth))
 
 inflacja = 2.324671717
-
-suma_marzec = (1 + ((inflacja + float(oprocentowanie))/1200)) * suma_luty - rata
-
-lastmonth = suma_luty - suma_marzec
-
-print("twoja pozostała kwota kredytu to " + str(suma_marzec))
-print("to " + str(lastmonth))
-print("mniej niż w poprzednim miesiącu \n")
+suma_marzec = round((1 + ((inflacja + float(oprocentowanie))/1200)) * suma_luty - rata, 2)
+lastmonth = round(suma_luty - suma_marzec, 2)
+print("Twoja pozostała kwota kredytu to {}, to {} mniej niż w poprzednim miesiąciu. \n".format(suma_marzec, lastmonth))
 
 inflacja = 1.261254407
-
-suma_kwiecien = (1 + ((inflacja + float(oprocentowanie))/1200)) * suma_marzec - rata
-
-lastmonth = suma_marzec - suma_kwiecien
-
-print("twoja pozostała kwota kredytu to " + str(suma_kwiecien))
-print("to " + str(lastmonth))
-print("mniej niż w poprzednim miesiącu \n")
+suma_kwiecien = round((1 + ((inflacja + float(oprocentowanie))/1200)) * suma_marzec - rata, 2)
+lastmonth = round(suma_marzec - suma_kwiecien, 2)
+print("Twoja pozostała kwota kredytu to {}, to {} mniej niż w poprzednim miesiąciu. \n".format(suma_kwiecien, lastmonth))
 
 inflacja = 1.782526286
-
-suma_maj = (1 + ((inflacja + float(oprocentowanie))/1200)) * suma_kwiecien - rata
-
-lastmonth = suma_kwiecien - suma_maj
-
-print("twoja pozostała kwota kredytu to " + str(suma_maj))
-print("to " + str(lastmonth))
-print("mniej niż w poprzednim miesiącu \n")
+suma_maj = round((1 + ((inflacja + float(oprocentowanie))/1200)) * suma_kwiecien - rata, 2)
+lastmonth = round(suma_kwiecien - suma_maj, 2)
+print("Twoja pozostała kwota kredytu to {}, to {} mniej niż w poprzednim miesiąciu. \n".format(suma_maj, lastmonth))
 
 inflacja = 2.329384541
-
-suma_czerwiec = (1 + ((inflacja + float(oprocentowanie))/1200)) * suma_maj - rata
-
-lastmonth = suma_maj - suma_czerwiec
-
-print("twoja pozostała kwota kredytu to " + str(suma_czerwiec))
-print("to " + str(lastmonth))
-print("mniej niż w poprzednim miesiącu \n")
-
+suma_czerwiec = round((1 + ((inflacja + float(oprocentowanie))/1200)) * suma_maj - rata, 2)
+lastmonth = round(suma_maj - suma_czerwiec, 2)
+print("Twoja pozostała kwota kredytu to {}, to {} mniej niż w poprzednim miesiąciu. \n".format(suma_czerwiec, lastmonth))
